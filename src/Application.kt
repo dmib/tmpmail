df0748b3e1952073f62a496678b2d150d17d6205
dmib <_._@_._>
2019-04-08 23:50:49 +0300

    third init commit

--- src/Application.kt
+++ src/Application.kt
@@ -1,20 +1,31 @@
 package by.example
 
 import freemarker.cache.ClassTemplateLoader
-import io.ktor.application.*
+import io.ktor.application.Application
+import io.ktor.application.call
+import io.ktor.application.install
 import io.ktor.freemarker.FreeMarker
 import io.ktor.freemarker.FreeMarkerContent
-import io.ktor.response.*
-import io.ktor.routing.*
-import io.ktor.http.*
-import io.ktor.html.*
-import kotlinx.html.*
-import kotlinx.css.*
+import io.ktor.response.respond
+import io.ktor.routing.get
+import io.ktor.routing.routing
+import io.ktor.sessions.*
+import org.apache.commons.lang3.RandomStringUtils
+import org.slf4j.LoggerFactory
 import java.util.*
-import javax.mail.Session
-import jdk.nashorn.internal.codegen.OptimisticTypesPersistence.store
 import javax.mail.Folder
+import javax.mail.Session
+import kotlin.collections.set
 
+
+private const val STRING_MAIL_LENGTH = 7
+private const val STRING_MAIL_PASSWORD = 20
+
+private const val SETUP_PATH = "setup_path"
+private const val DOMAIN = "domain"
+private const val COOKIE_MAIL = "mail"
+
+val logger = LoggerFactory.getLogger("ten-minutes-mail")
 
 fun main(args: Array<String>): Unit = io.ktor.server.tomcat.EngineMain.main(args)
 
@@ -26,14 +37,30 @@ fun Application.module(testing: Boolean = false) {
         templateLoader = ClassTemplateLoader(Application::class.java.classLoader, "templates")
     }
 
+    install(Sessions) {
+        cookie<MailAddress>(COOKIE_MAIL, storage = SessionStorageMemory())
+    }
+
 
     routing {
         get("/") {
-            val tempMail = generateTempMailAddress()
+            val tempMail = call.sessions.get<MailAddress>() ?: generateTempMailAddress()
             call.respond(
                 FreeMarkerContent(
                     "index.ftl",
-                    mapOf("mails" to checkEmail(tempMail.mail_address, tempMail.password), "tempMail" to tempMail), "e"
+                    mapOf("mails" to checkEmail(tempMail.mailAddress, tempMail.mailPassword), "tempMail" to tempMail),
+                    "e"
+                )
+            )
+            call.sessions.set(tempMail)
+        }
+        get("/mails") {
+            val tempMail = call.sessions.get<MailAddress>() ?: error("No mail found or expired")
+            call.respond(
+                FreeMarkerContent(
+                    "index.ftl",
+                    mapOf("mails" to checkEmail(tempMail.mailAddress, tempMail.mailPassword), "tempMail" to tempMail),
+                    "e"
                 )
             )
         }
@@ -44,32 +71,41 @@ class User(val userName: String, val email: String)
 
 class Mail(val subject: String, val message: String, val sender: String, val date: String)
 
-class MailAddress(val mail_address: String, val password: String)
+class MailAddress(val mailAddress: String, val mailPassword: String)
 
 
-fun generateTempMailAddress(): MailAddress = MailAddress("temp@abc.xyz", "password")
+fun generateTempMailAddress(): MailAddress {
+    val mail = "${RandomStringUtils.randomAlphabetic(STRING_MAIL_LENGTH)}@${System.getProperty(DOMAIN)}"
+    val mailPassword = RandomStringUtils.randomAlphabetic(STRING_MAIL_PASSWORD)
+    logger.info("User generate a new mail with login $mail and password $mailPassword")
+
+    val output = Runtime.getRuntime()
+        .exec(arrayOf("bash", "-c", "${System.getProperty(SETUP_PATH)} email add $mail $mailPassword")).waitFor()
+    if (output == 0) {
+        logger.info("Mail for user $mail generated")
+    }
+    return MailAddress(mail, mailPassword)
+}
 
 fun checkEmail(user: String, password: String): List<Mail> {
 
     val properties = Properties()
 
-    properties["mail.pop3.host"] = "example.com"
-    properties["mail.pop3.port"] = "995"
-    properties["mail.pop3.starttls.enable"] = "true"
+    properties["mail.pop3.port"] = "110"
     properties["mail.pop3.socketFactory.fallback"] = "false"
-    properties["mail.pop3.socketFactory.class"] = "javax.net.ssl.SSLSocketFactory"
-    properties["mail.pop3.socketFactory.port"] = "995"
+    properties["mail.pop3.socketFactory.port"] = "110"
 
-    val emailSession = Session.getDefaultInstance(properties)
-    val store = emailSession.getStore("pop3s")
+    val store = Session.getInstance(properties).getStore("pop3")
 
-    store.connect("example.com", user, password)
-    val emailFolder = store.getFolder("INBOX")
+    logger.info("Checking mails for user $user and password $password started")
+
+    store.connect(System.getProperty(DOMAIN), user, password)
+    val emailFolder = store.getFolder("Inbox")
     emailFolder.open(Folder.READ_ONLY)
 
     val messages = emailFolder.messages
 
-    emailFolder.close(false)
+    emailFolder.close(true)
     store.close()
 
     return messages.map { Mail(it.subject, it.content.toString(), it.from[0].toString(), it.sentDate.toString()) }
